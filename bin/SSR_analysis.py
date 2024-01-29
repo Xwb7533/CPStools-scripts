5a753b6c5b86b35dfbcbde04537ad5e88f8d2896
Xu Wenbo <93017074+Xwb7533@users.noreply.github.com>
2024-01-29 23:34:43 +0800

    Update SSR_analysis.py

--- bin/SSR_analysis.py
+++ bin/SSR_analysis.py
@@ -54,26 +54,32 @@ def find_SSRs(input_file, *length):
           f"Tetranucleotide:{k4}\n"
           f"Pentanucleotide:{k5}\n"
           f"Hexanucleotide:{k6}\n")
-
     my_seq = check_files(input_file)
-    match1 = re.finditer(r'([ATCG])\1{{{0},}}'.format(k1-1), my_seq)
-    match2 = re.finditer(r'(((?!AA|TT|CC|GG)[ATCG]){{2}})\1{{{0},}}'.format(k2-1), my_seq)
-    match3 = re.finditer(r'(((?!AAA|TTT|CCC|GGG)[ATCG]){{3}})\1{{{0},}}'.format(k3-1), my_seq)
-    match4 = re.finditer(r'(?!(\w)(\w)\1\2)(((?!AAAA|TTTT|CCCC|GGGG)[ATCG]){{4}})\3{{{0},}}'.format(k4-1), my_seq)
-    #match4 = re.finditer(r'(((?!AAAA|TTTT|CCCC|GGGG)[ATCG]){{4}})\1{{{0},}}'.format(k4-1), my_seq)
-    match5 = re.finditer(r'(((?!AAAAA|TTTTT|CCCCC|GGGGG)[ATCG]){{5}})\1{{{0},}}'.format(k5-1), my_seq)
-    match6 = re.finditer(r'(?!(\w)(\w)(\w)\1\2\3)(((?!AAAAAA|TTTTTT|CCCCCC|GGGGGG)[ATCG]){{6}})\4{{{0},}}'.format(k6-1), my_seq)
-    #match6 = re.finditer(r'(((?!AAAAAA|TTTTTT|CCCCCC|GGGGGG)[ATCG]){{6}})\1{{{0},}}'.format(k6-1), my_seq)
-
-    # 收集所有匹配结果
+    matches = {
+    'match1' : re.finditer(r'([ATCG])\1{{{0},}}'.format(k1-1), my_seq),
+    'match2' : re.finditer(r'(((?!AA|TT|CC|GG)[ATCG]){{2}})\1{{{0},}}'.format(k2-1), my_seq),
+    'match3' : re.finditer(r'(((?!AAA|TTT|CCC|GGG)[ATCG]){{3}})\1{{{0},}}'.format(k3-1), my_seq),
+    'match4' : re.finditer(r'(?!(\w)(\w)\1\2)(((?!AAAA|TTTT|CCCC|GGGG)[ATCG]){{4}})\3{{{0},}}'.format(k4-1), my_seq),
+    'match5' : re.finditer(r'(((?!AAAAA|TTTTT|CCCCC|GGGGG)[ATCG]){{5}})\1{{{0},}}'.format(k5-1), my_seq),
+    'match6' : re.finditer(r'(?!(\w)(\w)(\w)\1\2\3)(((?!AAAAAA|TTTTTT|CCCCCC|GGGGGG)[ATCG]){{6}})\4{{{0},}}'.format(k6-1), my_seq)
+    }
     all_matches = []
-    for match in match1, match2, match3, match4, match5, match6:
-        all_matches.extend(match)
+    for index_ in range(1, 7):
+        group_name = 'match' + str(index_)
+        for m in matches[group_name]:
+            start = m.start() + 1 
+            if not any(start == int(match.split('\t')[2]) for match in all_matches):
+                character = m.group()[:index_]
+                length = len(m.group()) // index_
+                end = m.end()           
+                output = f"{character}\t{length}\t{start}\t{end}"
+
+                print(output) 
+                all_matches.append(output)  
+
+
     # 按照span进行排序
-    all_matches = sorted(all_matches, key=lambda m: m.span())
-    cc, dd = [], []
-    for i in all_matches:
-        cc.append(i[0])
+    all_matches_sorted = sorted(all_matches, key=lambda x: int(x.split('\t')[2]))
     file_name = os.path.basename(input_file).split('.')[0]
     save_name = file_name + "_SSRs_results.txt"
     # save_name2 = file_name + "_SSRs_simplification.txt"
@@ -81,12 +87,8 @@ def find_SSRs(input_file, *length):
     # file_save2 = os.path.join(os.path.dirname(input_file), save_name2)
     with open(file_save, 'w') as ff:
         ff.write("type\tlength\tstart\tend\n")
-        for match_obj in all_matches:
-            repeat_unit = match_obj.group()  # 获取匹配到的具体内容
-            start_pos = match_obj.start()
-            end_pos = match_obj.end()
-            ssr_type = len(repeat_unit)  # SSR类型为匹配到的具体内容的长度
-            ff.write(f"{repeat_unit}\t{len(repeat_unit)}\t{start_pos+1}\t{end_pos}\n")
+        for SSRs_ in (all_matches_sorted):
+            ff.write(f"{SSRs_}\n")
     print(f"results:\ttotal {len(all_matches)} were detected! and the results was written into:\n\t\t"
         f"{os.path.abspath(file_save)}\n{'-'*80}")
 def main():
